351af6ae21a9bb22a9c4be3df0b399d9246a9127
Juan Carlos <juanjcsr@gmail.com>
2014-07-02 14:02:03 -0500

    assets para heroku

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.listen(port);
