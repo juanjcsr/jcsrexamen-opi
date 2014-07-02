bcf192ea5ce486f684d58d32b9dac6207e17ec8b
Juan Carlos <juanjcsr@gmail.com>
2014-07-02 13:29:53 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+ar express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(port);
---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name examenApp
+ * @description
+ * # examenApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('examenApp', [
+    'ngResource'
+  ]);
