f98a78ec53cdd874fe063d5c56402469f00a4532
Juan Carlos <juanjcsr@gmail.com>
2014-07-03 16:03:20 -0500

    agregar y autorizar actividades

--- app/scripts/modelos/modelos.js
+++ app/scripts/modelos/modelos.js
@@ -1,23 +1,27 @@
+/*'use scrict';
+(function(){
 var User = function(username, password, rol){
   this.username = username;
   this.password = password;
   this.rol = rol;
-}
+};
 
 var Activity = function(id, desc, monto, status) {
   this.id = id;
   this.description = desc;
   this.monto = monto;
   this.status = status;
-}
+};
 
 var Location = function(lat, lng) {
   this.latitude = lat;
   this.longitude = lng;
-}
+};
 
 var County = function(nombre, loc, activities) {
   this.nombre = nombre;
   this.location = loc;
   this.activities = activities;
-}
+};
+
+} */
